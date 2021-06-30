d0a183922c4607511213228acfa7d593666db7e0
starcrazzyluck <>
2021-06-30 19:23:16 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,35 @@
+-- Exibe uma mensagem.
+print("Hello World")
+
+--Somando
+print("1+1=")
+print(1+1)
+
+--Operadores de comparação
+print(2<3)
+print(4>7)
+print(4<=6)
+print(10>=110)
+
+--Variáveis 
+menssage1 = "This is a message" 
+print(menssage1)
+randomNumber = 19231923124
+print(randomNumber)
+
+part1 = "Part1"
+part2 = "Part2"
+print(part1 .. " " .. part2)
+
+price = 10
+priceMessage = "Esse tênis custa:"
+print(priceMessage .. " " .. price .. "R$")
+
+bool = true
+print("O valor está: " .. tostring(bool == false)) -- Converte o valor
+nilTest = nil
+print(nilTest)
+strings = "Oi\nTudo Bem?"
+print(strings)
+tabulacao = "\t TESTE"
+print(tabulacao)
